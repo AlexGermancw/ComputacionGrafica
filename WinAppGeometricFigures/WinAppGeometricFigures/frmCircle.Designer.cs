7a077676ec01ec8a0f7785ce6bb92caf12b8248e
AlexGermanC <agchicaiza2@espe.edu.ec>
2020-12-06 15:54:55 -0500

    Primera Clase

---
+++ WinAppGeometricFigures/WinAppGeometricFigures/frmCircle.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace WinAppGeometricFigures
+{
+    partial class frmCircle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbInputs = new System.Windows.Forms.GroupBox();
+            this.grbProcess = new System.Windows.Forms.GroupBox();
+            this.grbOutputs = new System.Windows.Forms.GroupBox();
+            this.grbCanvas = new System.Windows.Forms.GroupBox();
+            this.lblRadius = new System.Windows.Forms.Label();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.btmCalculate = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblPerimeter = new System.Windows.Forms.Label();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.txtPerimeter = new System.Windows.Forms.TextBox();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.grbInputs.SuspendLayout();
+            this.grbProcess.SuspendLayout();
+            this.grbOutputs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grbInputs
+            // 
+            this.grbInputs.Controls.Add(this.txtRadius);
+            this.grbInputs.Controls.Add(this.lblRadius);
+            this.grbInputs.Location = new System.Drawing.Point(23, 26);
+            this.grbInputs.Name = "grbInputs";
+            this.grbInputs.Size = new System.Drawing.Size(364, 93);
+            this.grbInputs.TabIndex = 0;
+            this.grbInputs.TabStop = false;
+            this.grbInputs.Text = "Entradas";
+            // 
+            // grbProcess
+            // 
+            this.grbProcess.Controls.Add(this.btnReset);
+            this.grbProcess.Controls.Add(this.btmCalculate);
+            this.grbProcess.Location = new System.Drawing.Point(23, 138);
+            this.grbProcess.Name = "grbProcess";
+            this.grbProcess.Size = new System.Drawing.Size(364, 90);
+            this.grbProcess.TabIndex = 1;
+            this.grbProcess.TabStop = false;
+            this.grbProcess.Text = "Procesos";
+            // 
+            // grbOutputs
+            // 
+            this.grbOutputs.Controls.Add(this.txtArea);
+            this.grbOutputs.Controls.Add(this.txtPerimeter);
+            this.grbOutputs.Controls.Add(this.lblArea);
+            this.grbOutputs.Controls.Add(this.lblPerimeter);
+            this.grbOutputs.Location = new System.Drawing.Point(23, 252);
+            this.grbOutputs.Name = "grbOutputs";
+            this.grbOutputs.Size = new System.Drawing.Size(364, 170);
+            this.grbOutputs.TabIndex = 2;
+            this.grbOutputs.TabStop = false;
+            this.grbOutputs.Text = "Salida";
+            // 
+            // grbCanvas
+            // 
+            this.grbCanvas.Location = new System.Drawing.Point(393, 26);
+            this.grbCanvas.Name = "grbCanvas";
+            this.grbCanvas.Size = new System.Drawing.Size(523, 396);
+            this.grbCanvas.TabIndex = 3;
+            this.grbCanvas.TabStop = false;
+            this.grbCanvas.Text = "Grafico";
+            // 
+            // lblRadius
+            // 
+            this.lblRadius.AutoSize = true;
+            this.lblRadius.Location = new System.Drawing.Point(66, 40);
+            this.lblRadius.Name = "lblRadius";
+            this.lblRadius.Size = new System.Drawing.Size(49, 17);
+            this.lblRadius.TabIndex = 0;
+            this.lblRadius.Text = "Radio:";
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(165, 40);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(105, 22);
+            this.txtRadius.TabIndex = 1;
+            // 
+            // btmCalculate
+            // 
+            this.btmCalculate.Location = new System.Drawing.Point(6, 47);
+            this.btmCalculate.Name = "btmCalculate";
+            this.btmCalculate.Size = new System.Drawing.Size(109, 24);
+            this.btmCalculate.TabIndex = 0;
+            this.btmCalculate.Text = "Calcular";
+            this.btmCalculate.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(121, 47);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(109, 24);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Resetear";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(259, 185);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(109, 24);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Salir";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // lblPerimeter
+            // 
+            this.lblPerimeter.AutoSize = true;
+            this.lblPerimeter.Location = new System.Drawing.Point(66, 46);
+            this.lblPerimeter.Name = "lblPerimeter";
+            this.lblPerimeter.Size = new System.Drawing.Size(69, 17);
+            this.lblPerimeter.TabIndex = 0;
+            this.lblPerimeter.Text = "Perimetro";
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Location = new System.Drawing.Point(66, 97);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(38, 17);
+            this.lblArea.TabIndex = 1;
+            this.lblArea.Text = "Area";
+            // 
+            // txtPerimeter
+            // 
+            this.txtPerimeter.Location = new System.Drawing.Point(165, 47);
+            this.txtPerimeter.Name = "txtPerimeter";
+            this.txtPerimeter.Size = new System.Drawing.Size(104, 22);
+            this.txtPerimeter.TabIndex = 2;
+            // 
+            // txtArea
+            // 
+            this.txtArea.Location = new System.Drawing.Point(165, 94);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.Size = new System.Drawing.Size(104, 22);
+            this.txtArea.TabIndex = 3;
+            // 
+            // frmCircle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 441);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.grbCanvas);
+            this.Controls.Add(this.grbOutputs);
+            this.Controls.Add(this.grbProcess);
+            this.Controls.Add(this.grbInputs);
+            this.Name = "frmCircle";
+            this.Text = "frmCircle";
+            this.grbInputs.ResumeLayout(false);
+            this.grbInputs.PerformLayout();
+            this.grbProcess.ResumeLayout(false);
+            this.grbOutputs.ResumeLayout(false);
+            this.grbOutputs.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbInputs;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Label lblRadius;
+        private System.Windows.Forms.GroupBox grbProcess;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btmCalculate;
+        private System.Windows.Forms.GroupBox grbOutputs;
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.TextBox txtPerimeter;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label lblPerimeter;
+        private System.Windows.Forms.GroupBox grbCanvas;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
---
+++ WinAppGeometricFigures/WinAppGeometricFigures/frmMenu.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace WinAppGeometricFigures
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbMenu = new System.Windows.Forms.GroupBox();
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.btnSquare = new System.Windows.Forms.Button();
+            this.btnRectangle = new System.Windows.Forms.Button();
+            this.btnTriangle = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.grbMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grbMenu
+            // 
+            this.grbMenu.Controls.Add(this.btnExit);
+            this.grbMenu.Controls.Add(this.btnTriangle);
+            this.grbMenu.Controls.Add(this.btnRectangle);
+            this.grbMenu.Controls.Add(this.btnSquare);
+            this.grbMenu.Controls.Add(this.btnCircle);
+            this.grbMenu.Location = new System.Drawing.Point(17, 12);
+            this.grbMenu.Name = "grbMenu";
+            this.grbMenu.Size = new System.Drawing.Size(372, 339);
+            this.grbMenu.TabIndex = 0;
+            this.grbMenu.TabStop = false;
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.Location = new System.Drawing.Point(48, 21);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(280, 43);
+            this.btnCircle.TabIndex = 0;
+            this.btnCircle.Text = "Circulo";
+            this.btnCircle.UseVisualStyleBackColor = true;
+            this.btnCircle.Click += new System.EventHandler(this.btnCircle_Click);
+            // 
+            // btnSquare
+            // 
+            this.btnSquare.Location = new System.Drawing.Point(48, 151);
+            this.btnSquare.Name = "btnSquare";
+            this.btnSquare.Size = new System.Drawing.Size(280, 43);
+            this.btnSquare.TabIndex = 1;
+            this.btnSquare.Text = "Cuadrado";
+            this.btnSquare.UseVisualStyleBackColor = true;
+            this.btnSquare.Click += new System.EventHandler(this.btnSquare_Click);
+            // 
+            // btnRectangle
+            // 
+            this.btnRectangle.Location = new System.Drawing.Point(48, 85);
+            this.btnRectangle.Name = "btnRectangle";
+            this.btnRectangle.Size = new System.Drawing.Size(280, 43);
+            this.btnRectangle.TabIndex = 2;
+            this.btnRectangle.Text = "Retangulo";
+            this.btnRectangle.UseVisualStyleBackColor = true;
+            this.btnRectangle.Click += new System.EventHandler(this.btnRectangle_Click);
+            // 
+            // btnTriangle
+            // 
+            this.btnTriangle.Location = new System.Drawing.Point(48, 212);
+            this.btnTriangle.Name = "btnTriangle";
+            this.btnTriangle.Size = new System.Drawing.Size(280, 43);
+            this.btnTriangle.TabIndex = 3;
+            this.btnTriangle.Text = "Triangulo";
+            this.btnTriangle.UseVisualStyleBackColor = true;
+            this.btnTriangle.Click += new System.EventHandler(this.btnTriangle_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(48, 272);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(280, 43);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Salir";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grbMenu);
+            this.Name = "frmMenu";
+            this.Text = "Form1";
+            this.grbMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbMenu;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnTriangle;
+        private System.Windows.Forms.Button btnRectangle;
+        private System.Windows.Forms.Button btnSquare;
+        private System.Windows.Forms.Button btnCircle;
+    }
+}
+
